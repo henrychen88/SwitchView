1d27fdf8bccd69b70c2b0a0f5b8690aaf2adb0a8
henrychen88 <24129114@qq.com>
2014-07-09 11:08:22 +0800

    Initial Commit

---
+++ SwitchView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SwitchView
+//
+//  Created by Henry on 14-7-9.
+//  Copyright (c) 2014年 Surwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SwitchViewTests/SwitchViewTests.m
@@ -0,0 +1,34 @@
+//
+//  SwitchViewTests.m
+//  SwitchViewTests
+//
+//  Created by Henry on 14-7-9.
+//  Copyright (c) 2014年 Surwin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwitchViewTests : XCTestCase
+
+@end
+
+@implementation SwitchViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
